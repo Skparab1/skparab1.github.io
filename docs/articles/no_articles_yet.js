e377c8687b2382d3a33fefaa2ac5ac74a50e9631
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-11-04 11:28:40 -0700

    Add files via upload

---
+++ docs/articles/no_articles_yet.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(1023,448);
+}
+
+
+function draw() {
+  background(0);
+  textSize(90);
+  fill(255);
+  text('No Articles yet!',300,200);
+  textSize(50);
+  text('Check back later!',300,300);
+}
