4590fd36b27971c023c6c5546e7b009356dabff3
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-11-06 09:14:17 -0700

    fixed 404 bug

--- docs/search/404.js
+++ docs/search/404.js
@@ -4,12 +4,15 @@ function setup() {
 
 loc = window.location.href;
 loc = str(loc);
+var t = 0;
   
 function draw() {
   
   if (loc.includes('https://skparab1.github.io/search/') && (t == 0)){
   query = loc.replace('https://skparab1.github.io/search=','');
   }
+  
+  t += 1;
   
   background(0);
   fill(255);
