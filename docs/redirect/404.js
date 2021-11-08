4ea908348f0b010556c7715a7d665eee1e6a8908
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-11-08 13:29:30 -0800

    Add files via upload

---
+++ docs/redirect/404.js
@@ -0,0 +1,41 @@
+function setup() {
+
+}
+
+function preload() {
+  redirect = loadTable("redirects.csv","csv","header");
+}
+
+keyword = redirect.getColumn(0);
+url = redirect.getColumn(1);
+
+
+loc = window.location.href;
+
+var foundredirect = false;
+  
+if (loc.includes('https://skparab1.github.io/redirect/')){
+  red = loc.replace('https://skparab1.github.io/redirect/','');
+  
+  i = 0;
+  scanner = '';
+  while (i <= keyword.length && foundredirect == false){
+    scanner = keyword[i];
+    if (scanner == red){
+      foundredirect = true;
+      openwindow = url[i];
+      openwindow = 'https://'+openwindow;
+    }
+    
+    i += 1;
+  }
+  
+  if (foundredirect){
+    location.href = openwindow;
+    window.open(openwindow,"_self");
+  } else {
+    window.open("http:skparab1.github.io/404","_self");
+  }
+} else {
+  window.open("http:skparab1.github.io/404","_self");
+}
