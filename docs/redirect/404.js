bd4a5e0b9c1dfe1f5c07ba3e539956cde6add0e9
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-11-13 09:53:41 -0800

    fixing redirect bug

--- docs/redirect/404.js
+++ docs/redirect/404.js
@@ -9,7 +9,8 @@ function setup() {
   createCanvas(1023,430);  
   keyword = redirect.getColumn(0);
   url = redirect.getColumn(1);
-  
+} 
+
 loc = window.location.href;
 
 var foundredirect = false;
@@ -34,59 +35,45 @@ if (loc.includes('https://skparab1.github.io/redirect/')){
   }
   
   if (foundredirect){
-    //location.href = openwindow;
+    location.href = openwindow;
     window.open(openwindow,"_self");
   } else {
     window.open("http:skparab1.github.io/wrongredirect","_self");
   }
   
-  while (true){
-    background(0);
-    textSize(25);
-    stroke(0);
-    fill(255);
-    strokeWeight(3);
-    
-    text('Redirecting to your requested webpage......',100,75);
-    text('This shouldn\'t take too long',100,110);
-    strokeWeight(8);
-    
-    stroke(255-Math.abs(0-tickercount));
-    line(500,200,500,150);
-    stroke(255-Math.abs(255-tickercount));
-    line(500,200,525,175);
-    stroke(255-Math.abs(510-tickercount));
-    line(500,200,550,200);
-    stroke(255-Math.abs(765-tickercount));
-    line(500,200,525,225);
-    stroke(255-Math.abs(1020-tickercount));
-    line(500,200,500,250);
-    stroke(255-Math.abs(1275-tickercount));
-    line(500,200,475,225);
-    stroke(255-Math.abs(1530-tickercount));
-    line(500,200,450,200);
-    
-    if (tickercount > 0){
-      stroke(255-Math.abs(1785-tickercount));
-      line(500,200,475,175);
-    } else {
-      stroke(255-Math.abs(-255-tickercount));
-      line(500,200,475,175);
-    }
-    
-    tickercount += 200;
-    if (tickercount > 1785+225+127.5){
-      tickercount = 0;
-    }
+  
+  background(0);
+  textSize(25);
+  stroke(0);
+  fill(255);
+  strokeWeight(3);
+
+  text('Redirecting to your requested webpage......',100,75);
+  text('This shouldn\'t take too long',100,110);
+  strokeWeight(8);
+
+  stroke(255);
+  line(500,200,500,150);
+  stroke(225);
+  line(500,200,525,175);
+  stroke(195);
+  line(500,200,550,200);
+  stroke(165);
+  line(500,200,525,225);
+  stroke(135);
+  line(500,200,500,250);
+  stroke(105);
+  line(500,200,475,225);
+  stroke(75);
+  line(500,200,450,200);
+  stroke(25);
+  line(500,200,475,175);
+
+  tickercount += 200;
+  if (tickercount > 1785+225+127.5){
+    tickercount = 0;
   }
   
 } else {
   window.open("http:skparab1.github.io/wrongredirect","_self");
 }
-}
-
-
-//function draw(){
-  
-
-//}
