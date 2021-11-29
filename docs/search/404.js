35cf3e4abf71edff78629a2b758d5efdc6c1b898
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-11-28 19:15:07 -0800

    push prototype

--- docs/search/404.js
+++ docs/search/404.js
@@ -7,20 +7,30 @@ function sortbase(array,base,max){
   let h = 0;
   let scan;
   let arrayn = array;
+  let overrider = 0;
   console.log('max'+max);
-  // i think i know what the bug is. its running out of elements in the source list
-  //                    0                             1                    2                              3                         4               5                              0              0            0                    0                      0
-  //var titles = ['Encryption code GUI Webpage','Encryption code','Skparab1\'s GitHub website','Runestone solutions Ps8','Encryption code GUI','Skparab1\'s GitHub profile','EzPixel Pro','Python samples','Javascript inkball','Skparab1 animation','Binary converter','Ml word engine','Encryption code graphics website','Randomizing algorithm','Encryption code website','Smart calculator','Pong','Game codes','Spanish English translator','Crossy road','Battleship','Game animation algorithms','Age calculator','Data organizers','Dino game','Space shooter game','HTML animation','Math codes','Flappy bird','Old Pascal program','AI virtual assistant','Snake game','Virtual assistant builder','JS url shortener','Loading animations','Game server','Traffic simulator'];
+  // ok, so it skips an index if its a comma right so we need to make it not skip the index of the regular ones
   while (newarray.length != arrayn.length){
-    scan = base[h]; // what is the corresponding value in the clicked array
+    scan = base[h*overrider+1]; // what is the corresponding value in the clicked array (skipping the commas
+    if (scan == ','){ // comma override
+      console.log('overrode');
+      //h += 1;
+      scan = base[h];
+      overrider = 2;
+    }
     // so its skipping one overy time
     if (scan == max){ newarray.push(arrayn[h]); console.log('pushed '+arrayn[h]);} 
-    if (h >= arrayn.length*2-1){ h = 0; max = max-1;}
+    if (h >= (arrayn.length*2-1)){ h = 0; max = max-1;}
     console.log(scan,max,h); if (max < 0){ break; } 
     h += 1;
     // in every other looping time, scan is a ',' so nan
   }
   return newarray;
+}
+
+function getpart(string,index){
+  const elements = string.split(' ');
+  return elements[index];
 }
 
 loc = window.location.href;
@@ -74,15 +84,15 @@ if (loc.includes('https://skparab1.github.io/search/')){
   langfilter = [true,true,true,true,true,true];
 }
 
-var keywords = ['','webhost webpage encryption code gui js javascript','encryption code text encrypting program in python','skparab1.github.io skparab1\'s github website written in javascript js','runestone solutions problem set 8 eight','gui of encryption code graphical user interface encrypt written in javascript js','skparab1 skparab1\'s github profile with links to projects','ezpixel pro high quality image editor in javascript js','python samples codes made in cs class runestone academy','javascript inkball microsoft video game made in javascript js','skparab animation for profile javascript js','binary converter decimal javascript js gui python script for cs class','ml word engine reconstructs words autocomplete','graphics website of encryption code','randomizing algorithm written in python random number generator','encryption code website','smart calculator perform various calculations','video game pong in python','game codes set of interactive','dynamic spanish english translator learn new entries command line interface','crossy road video game in python','board game battleship against computer artificial inteligence ai python','game animation algorithms use to animate objects free python','age calculator calculates in minutes hours seconds days weeks months years python','data organizers set of codes alphabetizer randomizer paragraph analyzer python','dino game google chorme no internet game python','space shooter game shoot objects python','html animation','math codes series command line interface','flappy bird video game in python','old pascal program','ai virtual assistant powered by typing python','snake game python','virtual assistant builder python','js javascript url shortener website redirect','loading animation animations js javascript','game server test testing game server peer p2p svelte','traffic simulator model win won hackathon 3rd third place js javascript graphics'];
+var keywords = ['','webhost webpage encryption code gui js javascript','encryption code text encrypting program in python','skparab1.github.io skparab1\'s github website written in javascript js','runestone solutions problem set 8 eight','gui of encryption code graphical user interface encrypt written in javascript js','skparab1 skparab1\'s github profile with links to projects','ezpixel pro high quality image editor in javascript js','python samples codes made in cs class runestone academy','javascript inkball microsoft video game made in javascript js','skparab animation for profile javascript js','binary converter decimal javascript js gui python script for cs class','ml word engine reconstructs words autocomplete','graphics website of encryption code','randomizing algorithm written in python random number generator','encryption code website','smart calculator perform various calculations','video game pong in python','game codes set of interactive','dynamic spanish english translator learn new entries command line interface','crossy road video game in python','board game battleship against computer artificial inteligence ai python','game animation algorithms use to animate objects free python','age calculator calculates in minutes hours seconds days weeks months years python','data organizers set of codes alphabetizer randomizer paragraph analyzer python','dino game google chorme no internet game python','space shooter game shoot objects python','html animation','math codes series command line interface','flappy bird video game in python','old pascal program','ai virtual assistant powered by typing python','snake game python','virtual assistant builder python','js javascript url shortener website redirect','loading animation animations js javascript','game server test testapi game server peer p2p svelte','traffic simulator model win won hackathon 3rd third place js javascript graphics'];
 var titles = ['','Encryption code GUI Webpage','Encryption code','Skparab1\'s GitHub website','Runestone solutions Ps8','Encryption code GUI','Skparab1\'s GitHub profile','EzPixel Pro','Python samples','Javascript inkball','Skparab1 animation','Binary converter','Ml word engine','Encryption code graphics website','Randomizing algorithm','Encryption code website','Smart calculator','Pong','Game codes','Spanish English translator','Crossy road','Battleship','Game animation algorithms','Age calculator','Data organizers','Dino game','Space shooter game','HTML animation','Math codes','Flappy bird','Old Pascal program','AI virtual assistant','Snake game','Virtual assistant builder','JS url shortener','Loading animations','Game server','Traffic simulator'];
 var urls = ['','skparab1.github.io/encryption-code-gui','github.com/skparab1/encryption-code','github.com/skparab1/skparab1.github.io','github.com/skparab1/runestone-solutions-ps8','github.com/skparab1/encryption-code-gui','github.com/skparab1','github.com/skparab1/ezpixel-pro','github.com/skparab1/python-samples','github.com/skparab1/javascript-inkball','github.com/skparab1/skparab1-animation','github.com/skparab1/binary-converter','github.com/skparab1/ml-word-engine','github.com/Encryptioncode/graphic-website','github.com/skparab1/randomizing-algorithm','github.com/skparab1/encryption-code-website','github.com/skparab1/master-math-calculator','github.com/skparab1/pong','github.com/skparab1/game-codes','github.com/skparab1/dynamic-spanish-english-translator','github.com/skparab1/corssy-road','github.com/skparab1/battleship','github.com/skparab1/game-animation-algorithms','github.com/skparab1/age-calculator','github.com/skparab1/data-organizers','github.com/skparab1/dino0-game','github.com/skparab1/space-shooter-game','github.com/skparab1/html-animation','github.com/skparab1/math-codes','github.com/skparab1/flappy-bird','github.com/skparab1/old-pascal-program','github.com/skparab1/ai-virtual-assistant','github.com/skparab1/snake-game','github.com/skparab1/virtual-assistant-builder','github.com/skparab1/js-url-shortener','github.com/skparab1/loading-animations','github.com/skparab1/game-server','github.com/skparab1/traffic-simulator'];
-var descriptions = ['','Live webpage of Encryption code GUI','Text encrypting program','Javascript GitHub website','Solutions to Runestone problem set 8 ','GUI of Encryption code','Skparab1\'s GitHub profile','High quality image editor','Sample python codes','The video game InkBall','Animation for Skparab1 profile','Binary <--> Decimal converter','Autocomplete word engine','Graphics website of Encryption code','Pseudo Random number generator','Website of Encryption code','Smart calculator','The Video game pong','Game codes','Dynamic Spanish English translator','Video game Crossy road','Board game Battleship','Game animation algorithms','Age calculator','Data organizing codes','Google chrome\'s no internet Dino game','Space shooter game','HTML animation','Math codes','The video game Flappy bird','Old Pascal program','AI virtual assistant','Snake game','Virtual assistant builder','JavaScript URL shortener, GitHub pages hostable','Loading animations for redirector','Testing game server with peerjs and svelte','3rd place hackathon winning Javascript traffic simulator'];
+var descriptions = ['','Live webpage of Encryption code GUI','Text encrypting program','Javascript GitHub website','Solutions to Runestone problem set 8 ','GUI of Encryption code','Skparab1\'s GitHub profile','High quality image editor','Sample python codes','The video game InkBall','Animation for Skparab1 profile','Binary <--> Decimal converter','Autocomplete word engine','Graphics website of Encryption code','Pseudo Random number generator','Website of Encryption code','Smart calculator','The Video game pong','Game codes','Dynamic Spanish English translator','Video game Crossy road','Board game Battleship','Game animation algorithms','Age calculator','Data organizing codes','Google chrome\'s no internet Dino game','Space shooter game','HTML animation','Math codes','The video game Flappy bird','Old Pascal program','AI virtual assistant','Snake game','Virtual assistant builder','JavaScript URL shortener, GitHub pages hostable','Loading animations for redirector','testapi game server with peerjs and svelte','3rd place hackathon winning Javascript traffic simulator'];
 var language = ['','JavaScript','Python','JavaScript','Python','JavaScript','Markdown','JavaScript','Python','JavaScript','JavaScript','JavaScript+Python','Python','JavaScript','Python','JavaScript','Python','Python','Python','Python','Python','Python','Python','Python','Python','Python','Python','HTML','Python','Python','Pascal','Python','Python','Python','JavaScript','JavaScript','JavaScript+Svelte','JavaScript'];
 var type = ['','Webpage','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software','Software'];
 
 // remember, the first one is insignificant
-var otherwise = ['',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var otherwise = ['',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 //let v = 0;  // so the array is ok now indexing does not include commas
 //while (v < otherwise.length){
@@ -93,11 +103,16 @@ var otherwise = ['',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 console.log(titles);
 console.log(otherwise);
 
-localStorage.setItem('clicklog',otherwise);
+//localStorage.setItem('testapi',otherwise);
 //var viewed = localstorage.getitem('viewlog') || otherwise; unnecassary for now
-var clicked = otherwise; //localStorage.getItem('clicklog') || 
+var clicked = localStorage.getItem('testapi'); // = otherwise; //localStorage.getItem('testapi') || 
+if (clicked == null){
+  localStorage.setItem('testapi',otherwise);
+  clicked = otherwise;
+}
 
-console.log(clicked);
+
+console.log('array that was gottn was ',clicked);
 
 //finding max
 var r = 0;
@@ -148,7 +163,7 @@ var results = [];
 var i = 0;
 var searching = false;
 var searchrender = query;
-var liveupdate = true;
+var testapiupdate = true;
 var clickpos = [];
 
 function returnis(q){
@@ -186,7 +201,8 @@ var unfilteredresults = [];
 var counter = 0;
   
 function draw() {
-  localStorage.setItem('clicklog',clicked);
+  console.log('writing to api: ',clicked);
+  localStorage.setItem('testapi',clicked);
   loc = str(loc);
   
   if (counter == 0){
@@ -219,13 +235,13 @@ function draw() {
     }
     
     if (clickpos[1] > ypos-33 && clickpos[1] < ypos+33 && clickpos[0] > 100 && clickpos[0] < 750){
-      let index = i;
-      let newval = clicked[index] + 1;
+      let index = i*2+1;
+      let newval = int(clicked[index]) + 1;
       clicked[index] = newval;
       console.log('newval '+newval+' but, val is '+clicked[index]);
       console.log('i '+i);
       console.log('should have changed'+clicked);
-      localStorage.setItem('clicklog',clicked);
+      localStorage.setItem('testapi',clicked);
       let openurl = 'https://'+urls[i];
       window.open(openurl);
     }
@@ -360,7 +376,7 @@ function draw() {
   stroke(0);
   
   textSize(18);
-  if (liveupdate){
+  if (testapiupdate){
     fill(200);
     rect(775,100,200,50);
     fill(0);
@@ -492,8 +508,8 @@ function mousePressed(){
   if (mouseX > 250 && mouseX < 450 && mouseY > 15 && mouseY < 70){
     searching = true;
   } else if (mouseX > 775 && mouseX < 995 && mouseY > 100 && mouseY < 150){
-    liveupdate = !liveupdate; 
-    if (liveupdate){
+    testapiupdate = !testapiupdate; 
+    if (testapiupdate){
       query = searchrender;
       results = [];
       unfilteredresults = [];
@@ -563,7 +579,7 @@ function keyPressed(){
     searchrender += key;
   }
   
-  if (liveupdate){
+  if (testapiupdate){
     query = searchrender;
     results = [];
     unfilteredresults = [];
@@ -581,7 +597,7 @@ function keyReleased(){
   }
   if ((keyCode == BACKSPACE || keyCode == DELETE) && searching){
     searchrender = searchrender.substring(0, searchrender.length -1);
-      if (liveupdate){
+      if (testapiupdate){
         query = searchrender;
         results = [];
         unfilteredresults = [];
