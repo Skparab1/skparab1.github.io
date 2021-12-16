7392aa36db720e123c6c981e466dee5d5de710b2
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-12-15 20:41:30 -0800

    added url redirect

--- docs/login/404.js
+++ docs/login/404.js
@@ -38,7 +38,7 @@ function setup() {
     
   } else {
     if (loc.includes('https://skparab1.github.io/login/')){
-      window.open('https://skparab1.github.io/login/auth=false&pwdchallenge=true&goto=https://skparab1.github.io',"_self");
+      window.open('https://skparab1.github.io/login/auth=false&pwdchallenge=true&goto=skparab1.github.io',"_self");
     }
   }
 }
@@ -179,7 +179,7 @@ function keyReleased(){
       let finder = 0;
       while (finder < usernames.length){
         if (username == usernames[finder] && password == passwords[finder] && logintimer == 0){
-          window.open(tolink,"_self");
+          window.open('https://'+tolink,"_self");
           found = true;
           logintimer += 1;
         }
