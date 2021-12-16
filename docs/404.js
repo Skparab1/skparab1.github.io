9e494be85ea6c0f38842658dd475c86a4f39dd36
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-12-16 13:34:21 -0800

    sus

--- docs/404.js
+++ docs/404.js
@@ -1,49 +1,24 @@
-function preload() {
-  login = loadTable("logins.csv","csv","header");
-}
-
-var usernames;
-var password;
-
-const getDeviceType = () => {
-  const ua = navigator.userAgent;
-  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-    return "tablet";
-  }
-  if (
-    /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
-      ua
-    )
-  ) {
-    return "mobile";
-  }
-  return "desktop";
-};
-
-const device = getDeviceType();
-
 function setup() {
-  if (device != 'mobile'){
-    createCanvas(windowWidth,windowHeight);
-  } else {
-    createCanvas(1023,420);
-  }
-  usernames = login.getColumn(0);
-  passwords = login.getColumn(1);
-  
-  loc = window.location.href;
-
-  if (loc.includes('https://skparab1.github.io/login/auth=false&pwdchallenge=true&goto=')){
-    tolink = loc.replace('https://skparab1.github.io/login/auth=false&pwdchallenge=true&goto=','');
-    
-  } else {
-    if (loc.includes('https://skparab1.github.io/login/')){
-      window.open('https://skparab1.github.io/login/auth=false&pwdchallenge=true&goto=home',"_self");
-    }
-  }
+  createCanvas(1023,430);  
 }
 
-console.log('done with setup');
+function preload(){
+  img404 = loadImage("404.png");
+}
+
+var counter = 0;
+var univcount = 0;
+var pos404 = 0;
+var direction = 'left';
+var speed = 50;
+var limit = 50;
+var allcount = 0;
+var rc = [];
+var t = 0;
+var shouldredirect = false;
+var tickercount = 0;
+var typingsearch = false;
+var query = '';
 
 var t = 0;
 var which;
@@ -58,172 +33,325 @@ var blue = 0;
 var changingcolor = 0;
 var changingsize = 0;
 var displayword = 0;
-var autorefresh = 0;
-var frate = 0;
-var code = '';
-var logintimer = 0;
-var dimmer = 0;
-var authinterface = 'authentication';
-var authenticator = 'username';
-var username = '';
-var password = '';
-var incorrect = 100;
-var backspacer = 0;
 
-function drawnicerect(x,y,xsize,ysize,incolor,outcolor){
-  let xchange = x-250;
-  let ychange = y-15;
-  let xsizechange = xsize-200;
-  let ysizechange = ysize-60;
-  
-  fill(incolor);
-  stroke(outcolor);
-  rect(250+xchange,15+ychange,xsize,ysize);
-  fill(outcolor);
-  stroke(outcolor);
-  rect(250+xchange,15+ychange,15,15);
-  rect(250+xchange,60+ychange+ysizechange,15,15);
-  rect(435+xchange+xsizechange,15+ychange,15,15);
-  rect(435+xchange+xsizechange,60+ychange+ysizechange,15,15);
-  fill(incolor);
-  stroke(incolor);
-  ellipse(265+xchange,30+ychange,24.5,24.5);
-  ellipse(435+xchange+xsizechange,30+ychange,24.5,24.5);
-  ellipse(265+xchange,60+ychange+ysizechange,24.5,24.5);
-  ellipse(435+xchange+xsizechange,60+ychange+ysizechange,24.5,24.5);
+loc = window.location.href; 
+
+if (loc.includes('https://skparab1.github.io/search=')){
+  query = loc.replace('https://skparab1.github.io/search=','');
+  localStorage.setItem('searchquery',query);
+  openwindow = "http://skparab1.github.io/search/" +query ;
+  window.open(openwindow,"_self");
+  console.log('should have redirected');
+  shouldredirect = true;
 }
-
-function draw(){
   
-  console.log('in draw');
-  resizeCanvas(windowWidth, windowHeight);
+function draw() {
   
-  if (keyIsDown(BACKSPACE) || keyIsDown(DELETE)){
-    backspacer += 1;
+  if (!shouldredirect){
+  
+  t += 1;
+  
+  fill(0);
+  rect(0,50,1023,330);
+  univcount += 1;
+  allcount += 1;
+  
+  if (counter == 0 || counter == 30){
+  rc = [random(150,255),random(150,255),random(150,255)]; }
+  
+  fill(0);
+  textSize(30);
+  text('ERROR 404',10,37);
+  text('ERROR 404',210,37);
+  text('ERROR 404',410,37);
+  text('ERROR 404',610,37);
+  text('ERROR 404',810,37);
+  fill(rc[1],rc[2],rc[0]);
+  textSize(45);
+  text('You BROKE it! I can\'t find that page!',100,100);
+  textSize(30);
+  text('But WAIT! Try these things!',300,140);
+  
+  image(img404,pos404-50,180,200,200);
     
-    if (backspacer % 7 == 0){
-      keyCode = BACKSPACE;
-      keyReleased();
-    }
-  }
-  
-  background(0);
-  
-  if (authenticator == 'username'){
-    drawnicerect(250,150,500,65,225,0);
-    drawnicerect(250,235,500,65,150,0);
+  fill(allcount*2,allcount*2-100,0);
+  rect(50,200,200,150);
+  fill(0,allcount*2,allcount*2-100);
+  rect(300,200,200,150);
+  fill(allcount*2-100,0,allcount*2);
+  rect(800,200,200,50);
+  if (!typingsearch){
+    fill(allcount*2-100,150,200);
   } else {
-    drawnicerect(250,150,500,65,150,0);
-    drawnicerect(250,235,500,65,225,0);
+    fill(allcount*2-100,allcount*2,allcount*2);
   }
-  fill(0,255-dimmer,100-dimmer);
-  stroke(0,0,0);
-  textSize(50);
-  text('Login to skparab1 services',100,75);
-  textSize(35);
-  text('Please Authenticate',300,120);
-  text('Username        ',75,190);
-  text('Password        ',75,280);
-  fill(255-dimmer,0,0);
-  stroke(200-dimmer);
-  text(username,260,195);
+  rect(800,260,200,75);
   
-  incorrect += 1;
-  if (incorrect < 100){
-    stroke(0);
-    fill(255,0,0);
-    textSize(20);
-    text('Username or password incorrect',260,320);
-  }
+  textSize(40);
+  fill(0);
+  text('GitHub',80,220+50);
+  text('Website',80,250+60);
   
-  stroke(255);
-  textSize(35);
-  let displaypass = '';
-  let y = 0;
-  while (y < password.length){
-    displaypass += '•';
-    y += 1;
-  }
-  text(displaypass,260,195+90);
+  text('GitHub',80+255,220+50);
+  text('Profile',80+255,250+60);
   
-  if (username != '' && password != ''){
-    drawnicerect(380,335,240,65,[255,200,0],0);
-    fill(0);
-    stroke(255,200,0);
-    text('Authenticate',405,380);
+  textSize(17);
+  
+  text('Lookup your url search',80+300+300+135,230);
+  text('Lookup something else',80+300+300+135,250+30);
+  text(query,80+300+300+135,250+60);
+  line(80+300+300+135,325,80+300+450+135,325);
+  
+  if (univcount < 200){
+    pos404 = pos404 + (600-pos404)/3 ;
+  } else if (univcount > 300){
+    pos404 = pos404 + (pos404-599)/3; }
+  if (univcount > 350){
+    univcount = 0;
+    pos404 = 0 ;
   }
   
-}
-
-function keyPressed(){
-  if (keyCode != BACKSPACE && keyCode != DELETE && keyCode != ENTER && key != 'Meta' && key != 'Alt' && key != 'Control' && key != 'Shift' && key != 'CapsLock' && key != 'Tab' && key != 'ArrowUp' && key != 'ArrowDown' && key != 'ArrowLeft' && key != 'ArrowRight'){
-    if (authinterface == 'authentication' && authenticator == 'username'){
-      username += key;
-    } else if (authinterface == 'authentication' && authenticator == 'password'){
-      password += key;
+  if (direction == 'left'){
+    //pos404 -= speed;
+  } else if (direction == 'right'){
+    //pos404 += speed;
+  }
+  
+  if (pos404 <= 450-limit){
+    direction = 'right';
+  } else if (pos404 >= 450+limit){
+    direction = 'left';
+  }
+  
+  if (counter == 0){
+  fill(random(255),random(255),random(255));
+  rect(0,0,200,50); }
+  if (counter == 2){
+  fill(random(255),random(255),random(255));
+  rect(200,0,200,50); }
+  if (counter == 4){
+  fill(random(255),random(255),random(255));
+  rect(400,0,200,50); }
+  if (counter == 6){
+  fill(random(255),random(255),random(255));
+  rect(600,0,200,50); }
+  if (counter == 8){
+  fill(random(255),random(255),random(255));
+  rect(800,0,200,50); }
+  if (counter == 10){
+  fill(random(255),random(255),random(255));
+  rect(1000,0,200,50); }
+  
+  if (counter == 30){
+  fill(random(255),random(255),random(255));
+  rect(0,380,200,50); }
+  if (counter == 32){
+  fill(random(255),random(255),random(255));
+  rect(200,380,200,50); }
+  if (counter == 34){
+  fill(random(255),random(255),random(255));
+  rect(400,380,200,50); }
+  if (counter == 36){
+  fill(random(255),random(255),random(255));
+  rect(600,380,200,50); }
+  if (counter == 38){
+  fill(random(255),random(255),random(255));
+  rect(800,380,200,50); }
+  if (counter == 40){
+  fill(random(255),random(255),random(255));
+  rect(1000,380,200,50); }
+  
+  counter += 1;
+  if (counter == 60){
+    counter = 0;
+  }  
+  } else {
+    if(t == 0){
+    which = Math.floor(Math.random() * 4);
+  }
+  t += 1;
+  
+  if (which == 1){
+  stroke(255-Math.abs(100-tickercount)+red/6,255-Math.abs(100-tickercount)+green/6,255-Math.abs(100-tickercount)+blue/6);
+  fill(255-Math.abs(100-tickercount)+red/6,255-Math.abs(100-tickercount)+green/6,255-Math.abs(100-tickercount)+blue/6);
+  ellipse(500,150,10,10);
+  
+  stroke(255-Math.abs(200-tickercount)+red/6,255-Math.abs(200-tickercount)+green/6,255-Math.abs(200-tickercount)+blue/6);
+  fill(255-Math.abs(200-tickercount)+red/6,255-Math.abs(200-tickercount)+green/6,255-Math.abs(200-tickercount)+blue/6);
+  ellipse(535.33,200-35.33,10,10);
+  
+  stroke(255-Math.abs(300-tickercount)+red/6,255-Math.abs(300-tickercount)+green/6,255-Math.abs(300-tickercount)+blue/6);
+  fill(255-Math.abs(300-tickercount)+red/6,255-Math.abs(300-tickercount)+green/6,255-Math.abs(300-tickercount)+blue/6);
+  ellipse(550,200,10,10);
+  
+  stroke(255-Math.abs(400-tickercount)+red/6,255-Math.abs(400-tickercount)+green/6,255-Math.abs(400-tickercount)+blue/6);
+  fill(255-Math.abs(400-tickercount)+red/6,255-Math.abs(400-tickercount)+green/6,255-Math.abs(400-tickercount)+blue/6);
+  ellipse(535.33,235.33,10,10);
+  
+  stroke(255-Math.abs(500-tickercount)+red/6,255-Math.abs(500-tickercount)+green/6,255-Math.abs(500-tickercount)+blue/6);
+  fill(255-Math.abs(500-tickercount)+red/6,255-Math.abs(500-tickercount)+green/6,255-Math.abs(500-tickercount)+blue/6);
+  ellipse(500,250,10,10);
+  
+  stroke(255-Math.abs(600-tickercount)+red/6,255-Math.abs(600-tickercount)+green/6,255-Math.abs(600-tickercount)+blue/6);
+  fill(255-Math.abs(600-tickercount)+red/6,255-Math.abs(600-tickercount)+green/6,255-Math.abs(600-tickercount)+blue/6);
+  ellipse(500-35.33,235.33,10,10);
+  
+  stroke(255-Math.abs(700-tickercount)+red/6,255-Math.abs(700-tickercount)+green/6,255-Math.abs(700-tickercount)+blue/6);
+  fill(255-Math.abs(700-tickercount)+red/6,255-Math.abs(700-tickercount)+green/6,255-Math.abs(700-tickercount)+blue/6);
+  ellipse(450,200,10,10);
+  
+  stroke(255-Math.abs(800-tickercount)+red/6,255-Math.abs(800-tickercount)+green/6,255-Math.abs(800-tickercount)+blue/6);
+  fill(255-Math.abs(800-tickercount)+red/6,255-Math.abs(800-tickercount)+green/6,255-Math.abs(800-tickercount)+blue/6);
+  ellipse(500-35.33,200-35.33,10,10);
+    
+  if (tickercount < 300){
+    stroke(255-Math.abs(-300-tickercount)+red/6,255-Math.abs(-300-tickercount)+green/6,255-Math.abs(-300-tickercount)+blue/6);
+    fill(255-Math.abs(-300-tickercount)+red/6,255-Math.abs(-300-tickercount)+green/6,255-Math.abs(-300-tickercount)+blue/6);
+    ellipse(500,250,10,10);
+    
+    stroke(255-Math.abs(-200-tickercount)+red/6,255-Math.abs(-200-tickercount)+green/6,255-Math.abs(-200-tickercount)+blue/6);
+    fill(255-Math.abs(-200-tickercount)+red/6,255-Math.abs(-200-tickercount)+green/6,255-Math.abs(-200-tickercount)+blue/6);
+    ellipse(500-35.33,235.33,10,10);
+  
+    stroke(255-Math.abs(-100-tickercount)+red/6,255-Math.abs(-100-tickercount)+green/6,255-Math.abs(-100-tickercount)+blue/6);
+    fill(255-Math.abs(-100-tickercount)+red/6,255-Math.abs(-100-tickercount)+green/6,255-Math.abs(-100-tickercount)+blue/6);
+    ellipse(450,200,10,10);
+    
+    stroke(255-Math.abs(tickercount)+red/6,255-Math.abs(tickercount)+green/6,255-Math.abs(tickercount)+blue/6);
+    fill(255-Math.abs(tickercount)+red/6,255-Math.abs(tickercount)+green/6,255-Math.abs(tickercount)+blue/6);
+    ellipse(500-35.33,200-35.33,10,10);    
+  }
+  
+  changingcolor -= 0.5;
+  if (changingcolor >= 1020 && false){
+    changingcolor = 255;
+  }
+  }
+  print(which);
+  
+  tickercount += 10;
+  if (tickercount > 600){
+    tickercount = -155;
+  }
+  ballbouncer += 7;
+  if (ballbouncer > 1500){
+    ballbouncer = -100;
+  }
+  
+  i = 0;
+  while (i < 3){
+    let h = 400-Math.abs((i*50)-ball2)-200;
+    if (h < 325-200){
+      fill(red,green,blue);
+      stroke(red,green,blue);
+      h = 325-200;
+    } else {
+      fill(red,green,blue);
+      stroke(red,green,blue);
     }
-  }
-  if (key == 'ArrowUp' || key == 'ArrowDown' || key == 'ArrowLeft' || key == 'ArrowRight'){
-    authinterface = 'authentication';
-  }
-}
-
-function keyReleased(){
-  if (keyCode == ENTER){
-    // go
-    if (authinterface == 'authentication' && authenticator == 'username'){
-      authenticator = 'password';
-      keyCode = '';
-    } else if (authinterface == 'authentication' && authenticator == 'password'){
-      let found = false;
-      let finder = 0;
-      while (finder < usernames.length){
-        if (username == usernames[finder] && password == passwords[finder] && logintimer == 0){
-          window.open(tolink,"_self");
-          found = true;
-          logintimer += 1;
-        }
-        finder += 1;
-      }
-      
-      if (!found){
-        incorrect = 0;
-      }
+    
+    if (which == 2){
+      ellipse(i*50+450,h+150,10,10);
     }
-  }
-  if (keyCode == BACKSPACE || keyCode == DELETE){
-    if (authinterface == 'authentication' && authenticator == 'username'){
-      username = username.substring(0, username.length -1);
-    } else if (authinterface == 'authentication' && authenticator == 'password'){
-      password = password.substring(0, password.length -1);
+    
+    h = 300 - h;
+    if (which == 0){
+    ellipse(i*50+450,h+95,10,10);
     }
-
+    i += 1;
+    
+  }
+  
+  changingsize += 2;
+  
+  if (which == 3){
+  
+  if (changingsize > 150){
+    changingsize = 0;
+  }
+  
+  let w = 30-Math.abs(25-changingsize);
+  if (w < 0){
+    w = 0;
+  }
+  
+  fill(0,100,255);
+  stroke(0,100,255);
+  ellipse(440,250,w,w);
+  
+  w = 30-Math.abs(50-changingsize);
+  if (w < 0){
+    w = 0;
+  }
+  
+  fill(255,0,0);
+  stroke(255,0,0);
+  ellipse(480,250,w,w);
+  
+  w = 30-Math.abs(75-changingsize);
+  if (w < 0){
+    w = 0;
+  }
+  
+  fill(255,255,0);
+  stroke(255,255,0);
+  ellipse(520,250,w,w);
+  
+  w = 30-Math.abs(100-changingsize);
+  if (w < 0){
+    w = 0;
+  }
+  
+  fill(0,225,50);
+  stroke(0,225,50);
+  ellipse(560,250,w,w);
+  }
+  
+  
+  changingcolor += 1;
+  if (changingcolor >= 1020){
+    changingcolor = 255;
+  }
+  
+  ball2 += 10;
+  if (ball2 > 300){
+    ball2 = -100;
+  }
+    
   }
 }
 
 function mousePressed(){
-  if (mouseX > 250 && mouseX < 750 && mouseY > 300 && mouseY < 365 && authinterface == 'default'){    
-    authinterface = 'authentication';
-  } else if (mouseX > 400 && mouseX < 600 && mouseY > 335 && mouseY < 400 && authinterface == 'authentication'){
-    authinterface = 'default';
-  } else if (mouseX > 250 && mouseX < 750 && mouseY > 150 && mouseY < 150+65 && authinterface == 'authentication'){    
-    authenticator = 'username';
-  } else if (mouseX > 250 && mouseX < 750 && mouseY > 235 && mouseY < 300 && authinterface == 'authentication'){    
-    authenticator = 'password';
-  } else if (mouseX > 380 && mouseX < 335 && mouseY > 380+240 && mouseY < 400 && authinterface == 'authentication'){    
-    let found = false;
-    let finder = 0;
-    while (finder < usernames.length){
-      if (username == usernames[finder] && password == passwords[finder] && logintimer == 0){
-        window.open(tolink,"_self");
-        found = true;
-        logintimer += 1;
-      }
-      finder += 1;
-    }
-    
-    if (!found){
-      incorrect = 0;
-    }
+  
+  if (mouseX > 50 && mouseX < 250 && mouseY > 200 && mouseY < 350){
+    window.open("http://skparab1.github.io","_self");
+  }
+  if (mouseX > 300 && mouseX < 500 && mouseY > 200 && mouseY < 350){
+    window.open("http://github.com/skparab1","_self");
+  }
+  if (mouseX > 800 && mouseX < 1000 && mouseY > 200 && mouseY < 250){
+    query = loc.replace('https://skparab1.github.io/','');
+    query = 'https://skparab1.github.io/search/'+query;
+    window.open(query,"_self");
+  }
+  if (mouseX > 800 && mouseX < 1000 && mouseY > 260 && mouseY < 260+75){
+    typingsearch = true;
+  }
+}
+
+function keyPressed(){
+  if (typingsearch && keyCode != BACKSPACE && keyCode != DELETE && keyCode != ENTER){
+    query += key;
+  }
+  
+  keyCode = '';
+}
+
+function keyReleased(){
+  if (keyCode == ENTER && typingsearch){
+    window.open('http://skparab1.github.io/search/'+query,"_self");
+  }
+  if ((keyCode == BACKSPACE || keyCode == DELETE) && typingsearch){
+    query = query.substring(0, query.length -1);
   }
 }
