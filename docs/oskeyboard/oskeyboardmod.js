c29f0d11d6acb5f3470ab6f8a68e18efa7cc7f26
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-12-22 13:42:59 -0800

    added full oskeyboard functionality

--- docs/oskeyboard/oskeyboardmod.js
+++ docs/oskeyboard/oskeyboardmod.js
@@ -1,19 +1,4 @@
-function setup() {
-  createCanvas(3000,1200);
-  //background(0);
-}
-
-import { displaykeyboard} from 'oskeyboard.js';
-
-var t = 1;
-var typed = '';
-var clickedpos = [0,0];
-var rerender = true;
-var rendertimer = 0;
-var capslock = false;
-var shift = false;
-
-function displaykeyboard(xpos,ypos,keysize) {
+export function displaykeyboard(xpos,ypos,keysize) {
   clear();
   let lightup = 'on';
   let xdiff = xpos - 100;
@@ -180,50 +165,4 @@ function displaykeyboard(xpos,ypos,keysize) {
   text(' Shift    Z    X    C    V    B    N    M    !    ?    @    &      SPACE',143+xdiff, (1225-846)*keysize+846+ydiff);
   
   clickedpos = [0,0];
-}
-
-function draw() {
-  
-  if (rendertimer < 2){
-    displaykeyboard(100,300,t);
-    console.log('rendered');
-  }
-  
-  rendertimer += 1;
-  //t -= 0.001;
-}
-
-function mousePressed(){
-  clickedpos = [mouseX,mouseY];
-  rendertimer = 0;
-}
-
-function keyPressed(){
-  if (keyCode != BACKSPACE && keyCode != DELETE && keyCode != ENTER && key != 'Meta' && key != 'Alt' && key != 'Control' && key != 'Shift' && key != 'CapsLock' && key != 'Tab' && key != 'ArrowUp' && key != 'ArrowDown' && key != 'ArrowLeft' && key != 'ArrowRight'){
-    if (capslock || shift){
-      typed += key.toUpperCase();
-      shift = false;
-    } else {
-      typed += key;
-    }
-    keyCode = '';
-  }
-  rendertimer = 0;
-}
-
-function keyReleased(){
-  if (keyCode == BACKSPACE || keyCode == DELETE){
-    typed = typed.substring(0, typed.length -1);
-    key = '';
-  }
-  if (keyCode == ENTER){
-    // idk do whatever you want
-    key = '';
-  }
-  if (keyCode == TAB){
-    // idk do whatever you want
-    // probably wont be that useful unless you confugure the browser
-    key = '';
-  }
-  rendertimer = 0;
 }
