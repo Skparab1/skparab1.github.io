b1c49135fa9cb3199226c2cb6828ed3a5ba69f9b
Shubham <71990977+Skparab1@users.noreply.github.com>
2022-02-23 10:08:15 -0800

    adda packer for search form

---
+++ docs/pack/dist/redirector.dev.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var loc = window.location.href;
+kew_found = loc.replace('https://skparab1.github.io/go/', '');
+r('test', 'https://www.google.com/search?q=test');
+r('clarke', 'https://docs.google.com/document/d/12t1be3KzO0hD-UEISfK49zdyhGyRm9AcGRyol9qg6UY/edit');
+r('pascal', 'https://skparab1.github.io/pascals_triangle');
+r('s', 'https://skparab1.github.io/search');
+r('traffic', 'https://skparab1.github.io/traffic_simulator');
+
+if (kew_found == 'test') {
+  window.open('https://www.google.com/search?q=test', "_self");
+}
+
+if (kew_found == 'clarke') {
+  window.open('https://docs.google.com/document/d/12t1be3KzO0hD-UEISfK49zdyhGyRm9AcGRyol9qg6UY/edit', "_self");
+}
+
+if (kew_found == 'pascal') {
+  window.open('https://skparab1.github.io/pascals_triangle', "_self");
+}
+
+if (kew_found == 's') {
+  window.open('https://skparab1.github.io/search', "_self");
+}
+
+if (kew_found == 'traffic') {
+  window.open('https://skparab1.github.io/traffic_simulator', "_self");
+}
+
+if (!foundredirect) {
+  window.open('https://skparab1.github.io/wrongredirect', "_self");
+}
---
+++ docs/pack/redirector.js
@@ -0,0 +1,46 @@
+var loc = window.location.href; //get the url
+data = loc.replace('https://skparab1.github.io/pack/',''); //extract the metadata
+
+//?search=game&py=true&md=true
+data = data.replace('?search=','/'); // reformat the search data
+
+// now reformat the filters
+if (data.includes('py=true')){     
+    data = data.replace('py=true','');
+} else {
+    data = data + 'py=false';
+}
+
+if (data.includes('js=true')){
+    data = data.replace('js=true','');
+} else {
+    data = data + 'js=false';
+}
+
+if (data.includes('html=true')){
+    data = data.replace('html=true','');
+} else {
+    data = data + 'html=false';
+}
+
+if (data.includes('pas=true')){
+    data = data.replace('pas=true','');
+} else {
+    data = data + 'pas=false';
+}
+
+if (data.includes('md=true')){
+    data = data.replace('md=true','');
+} else {
+    data = data + 'md=false';
+}
+
+if (data.includes('sv=true')){
+    data = data.replace('sv=true','');
+} else {
+    data = data + 'sv=false';
+}
+
+data = 'https://skparab1.github.io/search'+data;
+
+window.open(data,"_self");
