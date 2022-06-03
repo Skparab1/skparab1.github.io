dd2b26b47d3126b1bb39487d13360e29af737898
Shubham <71990977+Skparab1@users.noreply.github.com>
2022-06-02 20:06:04 -0700

    looks p cool

--- docs/skparab1_website_new/controller.js
+++ docs/skparab1_website_new/controller.js
@@ -5,7 +5,7 @@ var animator = 1000;
 var colorizer = 0;
 
 (async () => {
-  while (counter <= 10000){
+  while (counter <= 4000){
     colorizer = counter;
     if (colorizer >= 1000){
       colorizer = 1000;
@@ -19,29 +19,73 @@ var colorizer = 0;
       let tbutton = document.getElementById("top-button1");
       tbutton.style.top = (200+animator*0.5) + "px";
       tbutton.style.left = 0.04*window.innerWidth +"px";
-      tbutton.style.background = "rgba("+((colorizer/1000)*255)+","+((colorizer/1000)*100)+","+((colorizer/1000)*100)+","+(counter/1000)+")";
-      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*255)+","+((colorizer/1000)*100)+","+((colorizer/1000)*100)+","+(counter/1000)+")";
+      tbutton.style.background = "rgba("+((colorizer/1000)*255)+","+((colorizer/1000)*0)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*255)+","+((colorizer/1000)*0)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
 
       tbutton = document.getElementById("top-button2");
       tbutton.style.top = (200+animator) + "px";
       tbutton.style.left = 0.28*window.innerWidth +"px";
-      tbutton.style.background = "rgba("+((colorizer/1000)*255)+","+((colorizer/1000)*0)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
-      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*255)+","+((colorizer/1000)*0)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+      tbutton.style.background = "rgba("+((colorizer/1000)*200)+","+((colorizer/1000)*200)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*200)+","+((colorizer/1000)*200)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
 
       tbutton = document.getElementById("top-button3");
       tbutton.style.top = (200+animator*2) + "px";
       tbutton.style.left = 0.52*window.innerWidth +"px";
-      tbutton.style.background = "rgba("+((colorizer/1000)*255)+","+((colorizer/1000)*150)+","+((colorizer/1000)*50)+","+(counter/1000)+")";
-      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*255)+","+((colorizer/1000)*150)+","+((colorizer/1000)*50)+","+(counter/1000)+")";
+      tbutton.style.background = "rgba("+((colorizer/1000)*0)+","+((colorizer/1000)*255)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*0)+","+((colorizer/1000)*255)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
 
       tbutton = document.getElementById("top-button4");
       tbutton.style.top = (200+animator*3) + "px";
       tbutton.style.left = 0.76*window.innerWidth +"px";
-      tbutton.style.background = "rgba("+((colorizer/1000)*255)+","+((colorizer/1000)*200)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
-      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*255)+","+((colorizer/1000)*200)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+      tbutton.style.background = "rgba("+((colorizer/1000)*0)+","+((colorizer/1000)*0)+","+((colorizer/1000)*255)+","+(counter/1000)+")";
+      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*0)+","+((colorizer/1000)*0)+","+((colorizer/1000)*255)+","+(counter/1000)+")";
+
+      // next row
+
+      tbutton = document.getElementById("top-button5");
+      tbutton.style.top = (500+(animator)*4) + "px";
+      tbutton.style.left = 0.04*window.innerWidth +"px";
+      tbutton.style.background = "rgba("+((colorizer/1000)*200)+","+((colorizer/1000)*100)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*200)+","+((colorizer/1000)*100)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+
+      tbutton = document.getElementById("top-button6");
+      tbutton.style.top = (500+animator*5) + "px";
+      tbutton.style.left = 0.28*window.innerWidth +"px";
+      tbutton.style.background = "rgba("+((colorizer/1000)*200)+","+((colorizer/1000)*200)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*200)+","+((colorizer/1000)*200)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+
+      tbutton = document.getElementById("top-button7");
+      tbutton.style.top = (500+animator*6) + "px";
+      tbutton.style.left = 0.52*window.innerWidth +"px";
+      tbutton.style.background = "rgba("+((colorizer/1000)*0)+","+((colorizer/1000)*255)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*0)+","+((colorizer/1000)*255)+","+((colorizer/1000)*0)+","+(counter/1000)+")";
+
+      tbutton = document.getElementById("top-button8");
+      tbutton.style.top = (500+animator*7) + "px";
+      tbutton.style.left = 0.76*window.innerWidth +"px";
+      tbutton.style.background = "rgba("+((colorizer/1000)*0)+","+((colorizer/1000)*0)+","+((colorizer/1000)*255)+","+(counter/1000)+")";
+      tbutton.style.border = "2px solid rgba("+((colorizer/1000)*0)+","+((colorizer/1000)*0)+","+((colorizer/1000)*255)+","+(counter/1000)+")";
 
     }
     counter += 5;
+    animator = 0.99 * animator;
+    await sleep(2);
+  }
+
+  counter = 0;
+
+  animator = 1000;
+  while (counter < 1000){
+    tbutton = document.getElementById("top-button1");
+    tbutton.style.height = ((1000-animator)/1000*150)+100+"px";
+    tbutton = document.getElementById("top-button2");
+    tbutton.style.height = ((1000-(animator))/1000*150)+100+"px";
+    tbutton = document.getElementById("top-button3");
+    tbutton.style.height = ((1000-animator)/1000*150)+100+"px";
+    tbutton = document.getElementById("top-button4");
+    tbutton.style.height = ((1000-animator)/1000*150)+100+"px";
+    console.log('did it'+(1000-animator)/1000);
+    counter += 1;
     animator = 0.99 * animator;
     await sleep(2);
   }
--- docs/skparab1_website_new/dist/controller.dev.js
+++ docs/skparab1_website_new/dist/controller.dev.js
@@ -11,12 +11,13 @@ var animator = 1000;
 var colorizer = 0;
 
 (function _callee() {
-  var disp, disp2, tbutton;
+  var disp, disp2, _tbutton;
+
   return regeneratorRuntime.async(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
-          if (!(counter <= 10000)) {
+          if (!(counter <= 4000)) {
             _context.next = 10;
             break;
           }
@@ -35,26 +36,47 @@ var colorizer = 0;
               disp.style.background = "linear-gradient(rgb(" + 200 * counter / 1000 + ", " + 75 * counter / 1000 + ", 0),rgb(" + 200 * counter / 1000 + ", 0, 0),black)";
             }
 
-            tbutton = document.getElementById("top-button1");
-            tbutton.style.top = 200 + animator * 0.5 + "px";
-            tbutton.style.left = 0.04 * window.innerWidth + "px";
-            tbutton.style.background = "rgba(" + colorizer / 1000 * 255 + "," + colorizer / 1000 * 100 + "," + colorizer / 1000 * 100 + "," + counter / 1000 + ")";
-            tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 255 + "," + colorizer / 1000 * 100 + "," + colorizer / 1000 * 100 + "," + counter / 1000 + ")";
-            tbutton = document.getElementById("top-button2");
-            tbutton.style.top = 200 + animator + "px";
-            tbutton.style.left = 0.28 * window.innerWidth + "px";
-            tbutton.style.background = "rgba(" + colorizer / 1000 * 255 + "," + colorizer / 1000 * 0 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
-            tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 255 + "," + colorizer / 1000 * 0 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
-            tbutton = document.getElementById("top-button3");
-            tbutton.style.top = 200 + animator * 2 + "px";
-            tbutton.style.left = 0.52 * window.innerWidth + "px";
-            tbutton.style.background = "rgba(" + colorizer / 1000 * 255 + "," + colorizer / 1000 * 150 + "," + colorizer / 1000 * 50 + "," + counter / 1000 + ")";
-            tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 255 + "," + colorizer / 1000 * 150 + "," + colorizer / 1000 * 50 + "," + counter / 1000 + ")";
-            tbutton = document.getElementById("top-button4");
-            tbutton.style.top = 200 + animator * 3 + "px";
-            tbutton.style.left = 0.76 * window.innerWidth + "px";
-            tbutton.style.background = "rgba(" + colorizer / 1000 * 255 + "," + colorizer / 1000 * 200 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
-            tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 255 + "," + colorizer / 1000 * 200 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton = document.getElementById("top-button1");
+            _tbutton.style.top = 200 + animator * 0.5 + "px";
+            _tbutton.style.left = 0.04 * window.innerWidth + "px";
+            _tbutton.style.background = "rgba(" + colorizer / 1000 * 255 + "," + colorizer / 1000 * 0 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 255 + "," + colorizer / 1000 * 0 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton = document.getElementById("top-button2");
+            _tbutton.style.top = 200 + animator + "px";
+            _tbutton.style.left = 0.28 * window.innerWidth + "px";
+            _tbutton.style.background = "rgba(" + colorizer / 1000 * 200 + "," + colorizer / 1000 * 200 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 200 + "," + colorizer / 1000 * 200 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton = document.getElementById("top-button3");
+            _tbutton.style.top = 200 + animator * 2 + "px";
+            _tbutton.style.left = 0.52 * window.innerWidth + "px";
+            _tbutton.style.background = "rgba(" + colorizer / 1000 * 0 + "," + colorizer / 1000 * 255 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 0 + "," + colorizer / 1000 * 255 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton = document.getElementById("top-button4");
+            _tbutton.style.top = 200 + animator * 3 + "px";
+            _tbutton.style.left = 0.76 * window.innerWidth + "px";
+            _tbutton.style.background = "rgba(" + colorizer / 1000 * 0 + "," + colorizer / 1000 * 0 + "," + colorizer / 1000 * 255 + "," + counter / 1000 + ")";
+            _tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 0 + "," + colorizer / 1000 * 0 + "," + colorizer / 1000 * 255 + "," + counter / 1000 + ")"; // next row
+
+            _tbutton = document.getElementById("top-button5");
+            _tbutton.style.top = 500 + animator * 4 + "px";
+            _tbutton.style.left = 0.04 * window.innerWidth + "px";
+            _tbutton.style.background = "rgba(" + colorizer / 1000 * 200 + "," + colorizer / 1000 * 100 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 200 + "," + colorizer / 1000 * 100 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton = document.getElementById("top-button6");
+            _tbutton.style.top = 500 + animator * 5 + "px";
+            _tbutton.style.left = 0.28 * window.innerWidth + "px";
+            _tbutton.style.background = "rgba(" + colorizer / 1000 * 200 + "," + colorizer / 1000 * 200 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 200 + "," + colorizer / 1000 * 200 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton = document.getElementById("top-button7");
+            _tbutton.style.top = 500 + animator * 6 + "px";
+            _tbutton.style.left = 0.52 * window.innerWidth + "px";
+            _tbutton.style.background = "rgba(" + colorizer / 1000 * 0 + "," + colorizer / 1000 * 255 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 0 + "," + colorizer / 1000 * 255 + "," + colorizer / 1000 * 0 + "," + counter / 1000 + ")";
+            _tbutton = document.getElementById("top-button8");
+            _tbutton.style.top = 500 + animator * 7 + "px";
+            _tbutton.style.left = 0.76 * window.innerWidth + "px";
+            _tbutton.style.background = "rgba(" + colorizer / 1000 * 0 + "," + colorizer / 1000 * 0 + "," + colorizer / 1000 * 255 + "," + counter / 1000 + ")";
+            _tbutton.style.border = "2px solid rgba(" + colorizer / 1000 * 0 + "," + colorizer / 1000 * 0 + "," + colorizer / 1000 * 255 + "," + counter / 1000 + ")";
           }
 
           counter += 5;
@@ -67,6 +89,34 @@ var colorizer = 0;
           break;
 
         case 10:
+          counter = 0;
+          animator = 1000;
+
+        case 12:
+          if (!(counter < 1000)) {
+            _context.next = 28;
+            break;
+          }
+
+          tbutton = document.getElementById("top-button1");
+          tbutton.style.height = (1000 - animator) / 1000 * 150 + 100 + "px";
+          tbutton = document.getElementById("top-button2");
+          tbutton.style.height = (1000 - animator) / 1000 * 150 + 100 + "px";
+          tbutton = document.getElementById("top-button3");
+          tbutton.style.height = (1000 - animator) / 1000 * 150 + 100 + "px";
+          tbutton = document.getElementById("top-button4");
+          tbutton.style.height = (1000 - animator) / 1000 * 150 + 100 + "px";
+          console.log('did it' + (1000 - animator) / 1000);
+          counter += 1;
+          animator = 0.99 * animator;
+          _context.next = 26;
+          return regeneratorRuntime.awrap(sleep(2));
+
+        case 26:
+          _context.next = 12;
+          break;
+
+        case 28:
         case "end":
           return _context.stop();
       }
