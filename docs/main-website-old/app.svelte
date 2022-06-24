9e18b2c4928527800aed77c6cecb9ca891322fa2
Shubham Parab <71990977+Skparab1@users.noreply.github.com>
2022-06-24 09:19:09 -0700

    main website old

---
+++ docs/main-website-old/app.svelte
@@ -0,0 +1,28 @@
+<script>
+	let count = 0;
+	
+	$: if (count >= 10) {
+	alert('count is dangerously high!');
+	count = 9;
+		
+}
+
+	function handleClick() {
+		count += 1;
+		
+	}
+</script>
+
+{#each Array(6) as i}
+  <div class="row">
+   {#each Array(5) as j}
+      <div class="cell">
+        <input />
+      </div>
+   {/each}
+  </div>
+{/each}
+
+<button on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
